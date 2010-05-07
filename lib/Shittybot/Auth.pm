5b4c40ff08997f6fe27a0f51055ad250215ff6c4
ohwow <ohwow@cpan.org>
2010-05-07 23:23:03 +0400

    Session TTL is working now

--- lib/Shittybot/Auth.pm
+++ lib/Shittybot/Auth.pm
@@ -1,7 +1,9 @@
 package Shittybot::Auth;
 use 5.01;
+use strict;
+use warnings;
 use Moose;
-#use Shittybot::Auth::Session;
+use AnyEvent;
 use AnyEvent::IRC::Util qw/prefix_nick prefix_user prefix_host/;
 use Data::Dump  qw/ddx/;
 
@@ -29,16 +31,28 @@ sub parse_command {
   my ($command, @args) = split ' ' => $data;
   given($command) {
     when("auth") {
+      # todo: auth with sql tables, multiple accs, etc
       if ($args[0] eq $self->ownernick &&
 	  $args[1] eq $self->ownerpass) {
 	$self->{sessions}->{$args[0]} = Shittybot::Auth::Session->new(accountname => $args[0], host => $from);
+	$self->{sessions}->{$args[0]}->{timer} = AnyEvent->timer(
+		  after => $self->sessionttl,
+		  cb => sub {
+		    #ddx "In timer";
+		    delete $self->{sessions}->{$args[0]};
+		  }
+            );
 	return ("msg", "good job");
       } else {
 	return ("msg", "error, not authorised");
       }
     }
+    when("dump") {
+      ddx $self->{sessions};
+      return;
+    }
   }
-  $self->from = undef;
+#  $self->from = undef;
 }
 
 
@@ -48,4 +62,8 @@ package Shittybot::Auth::Session;
 use Moose;
 has accountname => (is => 'rw', isa => 'Str');
 has host => (is => 'rw', isa => 'Str');
+
+# TODO:
+#   CODE MORE
+
 1;
